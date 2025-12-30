7937b9b94387582dc7dfe1eeee7441bcf9e84a12
Pedro Bernardo <pedrober.rocha6@gmail.com>
2025-12-30 14:44:07 -0300

    Property/Encapsulamento

--- pythondev/POO_Projeto/modelos/biblioteca.py
+++ pythondev/POO_Projeto/modelos/biblioteca.py
@@ -4,7 +4,8 @@ class biblioteca:
     
     def __init__(self, nome):
         self.nome = nome
-        self.ativo = False
+        self._ativo = False  #Privado. Ao adicionar o _ eu torno ele em atributo privado e assim apenas a classe tem acesso para alterar o atributo
+        
         biblioteca.bibliotecas.append(self) # Adiciona a lista usando o self como palavra reservada
     
     def __str__(self):
@@ -13,8 +14,16 @@ class biblioteca:
     def lista ():
         for biblio in biblioteca.bibliotecas :
             print(f"{biblio.nome} | {biblio.ativo}")
+            
+    def alterar_estado(self):
+        self._ativo = not self._ativo
+        
+    @property
+    def ativo(self):
+        return "Ativada." if self._ativo else "Desativada." # Metodo set
     
 bibliotecaCidade = biblioteca("Biblioteca da Cidade") # Instancia o objeto. O nome agora e obrigatorio
+bibliotecaCidade.alterar_estado()
 bibliotecaCidade = biblioteca("Biblioteca do Shopping")
 
 biblioteca.lista()
